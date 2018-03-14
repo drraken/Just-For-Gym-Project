3da72235388acba64c9c4f610dbff3f3a984a1e8
Remigiusz Chiluta <remigiusz.x.chiluta@gsk.com>
2018-03-14 12:36:33 +0100

    project start

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var jade        = require('gulp-jade');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['jade','sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/jade/**/*.jade", ['jade']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("src/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
+// Compile jade into html files
+gulp.task('jade', function() {
+  return gulp.src("src/jade/*.jade")
+    .pipe(jade())
+    .pipe(gulp.dest("app/"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
