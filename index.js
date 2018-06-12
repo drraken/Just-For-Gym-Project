61450e5afb7b33a0e5f5746f3c316467593e4aec
Arkadiusz Chiluta <chiluta.arkadiusz@gmail.com>
2018-06-12 10:20:11 +0200

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path'),
   app = express(),
-  port = process.env.PORT || 5000,
+const PORT = process.env.PORT || 5000,
   mongoose = require('mongoose'),
   Task = require('./api/models/jfgModel'),
   bodyParser = require('body-parser');
