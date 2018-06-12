030039e7388170eb2d795207652eeff3c656e456
Arkadiusz Chiluta <chiluta.arkadiusz@gmail.com>
2018-06-12 10:50:49 +0200

    heroku

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-const express = require('express'),
-const path =require('path'),
-  app = express(),
-  port = process.env.PORT || 5000,
-  mongoose = require('mongoose'),
-  Task = require('./api/models/jfgModel'),
-  bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/JFG');
-
-mongoose.connection.once('open', function(){
-    console.log('Connection has been made, ')
-}).on('error', function(error){
-    console.log('Connection error:', error); 
-});
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var routes = require('./api/routes/jfgRoutes');
-routes(app);
-
-app.listen(port);
-
-console.log('JFG RESTful API server started on: ' + port);
-
-
- app.use(express.static(path.join(__dirname, 'public')));
- app.set('views', path.join(__dirname, 'views'));
- app.set('view engine', 'html');
- app.get('/', (req, res) => res.render('pages/index'));
-  
-
-  
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express'),
+const path =require('path'),
+  app = express(),
+  port = process.env.PORT || 5000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/jfgModel'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/JFG');
+
+mongoose.connection.once('open', function(){
+    console.log('Connection has been made, ')
+}).on('error', function(error){
+    console.log('Connection error:', error); 
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/jfgRoutes');
+routes(app);
+
+app.listen(port);
+
+console.log('JFG RESTful API server started on: ' + port);
+
+
+ app.use(express.static(path.join(__dirname, 'public')));
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'html');
+ app.get('/', (req, res) => res.render('pages/index'));
+  
+
+  
