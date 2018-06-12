b64cce0a5f1a7f5f7a9af8fbf355f06c216db88c
Arkadiusz Chiluta <chiluta.arkadiusz@gmail.com>
2018-06-12 11:25:49 +0200

    heroku

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'app')))
   .set('app', path.join(__dirname, 'app'))
-  //.set('view engine', 'html')
+  .set('view engine', 'Chrome HTML Document')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
