91c8ad7cc2f43d1fbb402c84e58084023712bcb8
Arkadiusz Chiluta <chiluta.arkadiusz@gmail.com>
2018-06-14 12:09:40 +0200

    heroku 2.

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ var   app = express();
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
 //
 
-app.use(express.static(__dirname + 'app'));
-app.set('views', __dirname );
+app.use(express.static(__dirname + ''));
+app.set('views', __dirname + 'app' );
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 
