578a161b6c4eea9df896311cd0254db5b8137331
Arkadiusz Chiluta <chiluta.arkadiusz@gmail.com>
2018-06-16 17:03:07 +0200

    heroku 3.0

--- server.js
+++ server.js
@@ -44,10 +44,13 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
    res.render('pages/index'); 
 });
+app.get('/login', function(req, res){
+   res.render('pages/login'); 
+});
 app.get('/mycart', function(req, res){
    res.render('pages/mycart'); 
 });
-app.get('/home', function(req, res){
+app.get('/login/x', function(req, res){
    res.render('pages/index'); 
 });
 app.get('/about', function(req, res){
