d1f0b36a1e6f91e1d40e7591ae5ffdacdec9c3b5
Arkadiusz Chiluta <chiluta.arkadiusz@gmail.com>
2018-06-17 22:31:53 +0200

    heroku upgrade

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 //pobiera dane z bazy danych
 function CheckTheData(l, p) {
-    fetch('http://localhost:30111/tasks')
+    fetch('http://localhost:3006/tasks')
         .then(response => {
             return response.json()
         })
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 30111;
+var port = process.env.PORT;
 var mongoose = require('mongoose');
 var Task = require('./api/models/jfgModel');
 var bodyParser = require('body-parser');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://draken:123qwe@ds161740.mlab.com:61740/justforgym');
-
+mongoose.connect('MONGODB_URI');
+//mongodb://draken:123qwe@ds161740.mlab.com:61740/justforgym
 mongoose.connection.once('open', function () {
     console.log('Connection has been made, ')
 }).on('error', function (error) {
