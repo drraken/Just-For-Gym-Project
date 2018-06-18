c61f4df2b5942882e1e870512de9f7ff6b96912a
Arkadiusz Chiluta <chiluta.arkadiusz@gmail.com>
2018-06-18 19:31:20 +0200

    making better

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var Task = require('./api/models/jfgModel');
 var bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://draken:123qwe@ds161740.mlab.com:61740/justforgym');
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.once('open', function () {
     console.log('Connection has been made, ')
 }).on('error', function (error) {
     console.log('Connection error:', error);
 });
-
+//'mongodb://draken:123qwe@ds161740.mlab.com:61740/justforgym');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
